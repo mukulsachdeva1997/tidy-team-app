024e2a71351f326a08f91002417f2e1d6d1fc121
mukulsachdeva1997 <mukulsachdeva1997@gmail.com>
2026-05-24 16:12:44 +0200

    Polish Tidy Team PWA features

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,7 +1,7 @@
 importScripts("https://www.gstatic.com/firebasejs/10.14.1/firebase-app-compat.js");
 importScripts("https://www.gstatic.com/firebasejs/10.14.1/firebase-messaging-compat.js");
 
-const APP_CACHE = "tidy-team-shell-v2";
+const APP_CACHE = "tidy-team-shell-v3";
 const APP_SHELL = ["/", "/index.html", "/manifest.json"];
 
 firebase.initializeApp({
